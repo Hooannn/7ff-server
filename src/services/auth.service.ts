128452714cd0197ae8394b43e8c668defb2429ff
Hooannn <hoanthui123@gmail.com>
2023-02-27 22:36:04 +0700

    fix: DRY

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -18,13 +18,13 @@ class AuthService {
   }
 
   public verifyRefreshToken(token: string) {
-    return this.jwt.verify(token, REFRESH_TOKEN_SECRET);
+    return this.jwt.verify(token, REFRESH_TOKEN_SECRET) as AuthJwtPayload;
   }
 
   public getAccessToken(oldAccessToken: string, refreshToken: string) {
     if (!refreshToken || !oldAccessToken) throw new HttpException(403, 'INVALID_TOKEN');
     const { userId: accessUserId, role } = this.jwt.decode(oldAccessToken) as AuthJwtPayload;
-    const { userId: refreshUserId } = this.jwt.verify(refreshToken, REFRESH_TOKEN_SECRET) as AuthJwtPayload;
+    const { userId: refreshUserId } = this.verifyRefreshToken(refreshToken);
     if (accessUserId !== refreshUserId) throw new HttpException(403, 'INVALID_TOKEN_INFO');
     return this.jwt.sign({ userId: accessUserId, role }, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_LIFE });
   }
